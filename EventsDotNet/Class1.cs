66fe57dd1f7044cd1e1c3fb44cc850513f0ec71b
Serabass <smaxutov@cic.kz>
2024-02-23 15:43:25 +0600

    1

---
+++ EventsDotNet/Class1.cs
@@ -0,0 +1,63 @@
+﻿namespace EventsDotNet;
+
+public class EventEmitter<I, O>
+{
+  public delegate void EmiterDelegate(I oldValue, I newValue);
+
+  public event EmiterDelegate Emitter;
+
+  public void On(Func<I, bool> condition, Action<I> cb)
+  {
+    Emitter += (o, n) =>
+    {
+      if (condition(n) && !condition(o))
+      {
+        cb(n);
+      }
+    };
+  }
+  
+  public void On(Func<I, I, bool> condition, Action<I> cb)
+  {
+    Emitter += (o, n) =>
+    {
+      if (condition(o, n))
+      {
+        cb(n);
+      }
+    };
+  }
+  
+  public void On(Func<I, I, Task<bool>> condition, Action<I> cb)
+  {
+    Emitter += async (o, n) =>
+    {
+      if (await condition(o, n))
+      {
+        cb(n);
+      }
+    };
+  }
+
+  public void On(Func<I, Task<bool>> condition, Action<I> cb)
+  {
+    Emitter += async (o, n) =>
+    {
+      if (await condition(n) && !await condition(o))
+      {
+        cb(n);
+      }
+    };
+  }
+
+  public void On<R, RI>(Func<RI> input, Func<RI, bool> condition, Action<I> cb)
+  {
+    Emitter += (o, n) =>
+    {
+      if (condition(input()))
+      {
+        cb(n);
+      }
+    };
+  }
+}
